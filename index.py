c0475a592509921ebb599994296fe8aa01586ee3
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2021-10-17 12:02:55 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,22 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = 'keys.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1e3q55ofcy5HGcwP0FZhbI4-hstxR1NOE-clh-sN7Hm8'
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="pyresponse!A1:C3").execute()
+
+values = result.get('values', [])
+# print(result)
